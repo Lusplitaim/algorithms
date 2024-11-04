5739d0f9adbed701285b4c19f3bb24790e361594
Lusplitaim <Lusplitaim@yandex.ru>
2024-11-04 18:41:40 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms.Search;
+using Bogus;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker();
+            List<int> nums = new();
+            for (int i = 0; i < 19; i++)
+            {
+                nums.Add(faker.Random.Int(1, 600));
+            }
+
+            var val = nums[3];
+            Console.WriteLine($"Searching for {val}");
+
+            nums.Sort();
+            var arr = nums.ToArray();
+            var idx = arr.TernarySearch(val);
+            PrintArray(arr);
+            Console.WriteLine($"Found at index [{idx}]");
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.Write("Array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Search/SearchingExtensions.cs
@@ -0,0 +1,80 @@
+﻿namespace Algorithms.Search
+{
+    internal static class SearchingExtensions
+    {
+        /*
+         * Best Case: O(1)
+         * Average Case: O(log N)
+         * Worst Case: O(log N)
+         */
+        public static int? BinarySearch(this int[] arr, int val)
+        {
+            int startIdx = 0;
+            int endIdx = arr.Length - 1;
+            while (startIdx <= endIdx)
+            {
+                int midIdx = startIdx + (endIdx - startIdx) / 2;
+                int mid = arr[midIdx];
+
+                if (mid == val)
+                {
+                    return midIdx;
+                }
+
+                if (val > mid)
+                {
+                    startIdx = midIdx + 1;
+                }
+                else
+                {
+                    endIdx = midIdx - 1;
+                }
+            }
+
+            return null;
+        }
+
+        /*
+         * Worst case: O(log3N)
+         * Average case: Θ(log3N)
+         * Best case: Ω(1)
+         */
+        public static int? TernarySearch(this int[] arr, int val)
+        {
+            int startIdx = 0;
+            int endIdx = arr.Length - 1;
+            while (startIdx <= endIdx)
+            {
+                int mid1Idx = startIdx + (endIdx - startIdx) / 3;
+                int mid1 = arr[mid1Idx];
+                int mid2Idx = endIdx - (endIdx - startIdx) / 3;
+                int mid2 = arr[mid2Idx];
+
+                if (mid1 == val)
+                {
+                    return mid1Idx;
+                }
+                if (mid2 == val)
+                {
+                    return mid2Idx;
+                }
+
+                if (val < mid1)
+                {
+                    endIdx = mid1Idx - 1;
+                }
+                else if (val > mid2)
+                {
+                    startIdx = mid2Idx + 1;
+                }
+                else
+                {
+                    startIdx = mid1Idx + 1;
+                    endIdx = mid2Idx - 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
